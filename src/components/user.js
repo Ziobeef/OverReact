bd44bae00ae77a9ce9031a3651523811fd8490a6
ziobeef <dimiziomikhael@gmail.com>
2025-08-06 20:17:19 +0700

    Implement create, update, and delete functionality for posts in User component

--- src/components/user.js
+++ src/components/user.js
@@ -5,8 +5,9 @@ import { useNavigate } from "react-router-dom";
 function User() {
   const [data, setData] = useState([]);
   const navigate = useNavigate();
+  const [isEditing, setIsEditing] = useState(false);
   const fetch = async () => {
-    const response = await axios.get("https://jsonplaceholder.typicode.com/posts?_limit=0");
+    const response = await axios.get("https://jsonplaceholder.typicode.com/posts?_limit=3");
     console.log(response);
     setData(response.data);
   };
@@ -14,9 +15,9 @@ function User() {
     e.preventDefault();
     const response = await axios.post("https://jsonplaceholder.typicode.com/posts", formData);
     setData([response.data, ...data]);
-     console.log(response);
-  
-  }
+    setformData({ title: "", body: "" });
+    console.log(response);
+  };
   const [formData, setformData] = useState({
     title: "",
     body: "",
@@ -30,9 +31,24 @@ function User() {
     });
   };
   const deleteData = async (id) => {
-   await axios.delete(`https://jsonplaceholder.typicode.com/posts/${id}`, formData);
-   setData(data.filter(e=>e.id !== id));
-  }           
+    await axios.delete(`https://jsonplaceholder.typicode.com/posts/${id}`, formData);
+    setData(data.filter((e) => e.id !== id));
+  };
+  const updateData = async (data) => {
+    setIsEditing(true);
+    setformData({
+      title: data.title,
+      body: data.body,
+      id: data.id,
+    });
+  };
+  const submitUpdateData = async (e) => {
+    e.preventDefault();
+    const response = await axios.put(`https://jsonplaceholder.typicode.com/posts/${formData.id}`, formData);
+    setData(data.map((item) => (item.id === formData.id ? response.data : item)));
+    setIsEditing(false);
+    setformData({ title: "", body: "" });
+  };
   useEffect(() => {
     fetch();
   }, []);
@@ -46,21 +62,32 @@ function User() {
             <div key={item.id}>
               <h4>{item.title}</h4>
               <h4>{item.body}</h4>
-              <button type="button" onClick={(e) => deleteData(item.id)}>Delete</button>
+              <div>id: {item.id}</div>
+              <button type="button" onClick={(e) => deleteData(item.id)}>
+                Delete Data
+              </button>
+              <button type="button" onClick={(e) => updateData(item)}>
+                Update Data
+              </button>
             </div>
           ))}
-        </div>  
+        </div>
       )}
 
       <div>
-        <form onSubmit={(e) => createPost(e)}>
-        <div>Judul</div>
-        <input name = "title" value={formData.title} onChange={(e) => handleMultiple(e)} />
-        <br/>
-        <div>Deskripsi</div>
-        <input name = "body" value={formData.body} onChange={(e) => handleMultiple(e)} />
-        <br/>
-        <button type="submit">buttion</button>
+        <form onSubmit={(e) => (isEditing ? submitUpdateData(e) : createPost(e))}>
+          <div>Judul</div>
+          <input name="title" value={formData.title} onChange={(e) => handleMultiple(e)} />
+          <br />
+          <div>Deskripsi</div>
+          <input name="body" value={formData.body} onChange={(e) => handleMultiple(e)} />
+          <br />
+          <button type="submit">{isEditing ? "Update Data" : "Submit Data"}</button>
+          {isEditing && (
+            <button type="button" onClick={() => (setIsEditing(false), setformData({ title: "", body: "" }))}>
+              Cancel
+            </button>
+          )}
         </form>
       </div>
     </div>
