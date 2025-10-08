00a34ad5ea0244efbfae362622cc235ac7a86a58
Ziobeef <dimiziomikhael@gmail.com>
2025-10-08 17:58:49 +0700

    Add OGL library for 3D rendering, implement background animation, and enhance profile component styling

--- src/components/profile.js
+++ src/components/profile.js
@@ -4,19 +4,17 @@ function Profile() {
   return (
     <div>
       <div>
-        <img src="/profile.jpg" />
+        <img className="blur" src="/profile.jpg" />
       </div>
-      <div className="container my-5">
-        <div className="row align-items-center">
-          <div className="col-6 border text-center ">
-            <h1>Hello Im Zio  </h1>
+      <div className="container my-5 position-absolute start-0 end-0" style={{ top: "30%" }}>
+        <div className="text-center border rounded-5 p-4  text-white mx-5" style={{ opacity: 0.6, backgroundColor: "#0b3835" }}>
+          <div className="fw-bold"> 
+            <h1>Hello Im Zio </h1>
             Maecenas rutrum condimentum lorem non ullamcorper. Nunc nec elementum est. Nunc gravida dui in tellus consequat eleifend. Proin imperdiet, est quis accumsan mollis, dui arcu tempus tellus, sed varius risus leo in eros. Aliquam
             ut odio sit amet neque pharetra suscipit. Suspendisse egestas quam nisi, quis porta quam tempus eu. Morbi nibh risus, pulvinar malesuada quam vel, porttitor tempor diam. Fusce facilisis dolor eget lorem commodo, ac sagittis
             lacus fermentum. Nunc condimentum metus quis imperdiet sollicitudin. Nunc elit diam, tempor vitae accumsan non, iaculis faucibus lacus.
           </div>
-          <div className="col-6 border ">
-            <img style={{ width: "60%" }} src="omjoko.jpg" />
-          </div>
+          <div></div>
         </div>
       </div>
     </div>
