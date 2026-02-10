7ba2d25e6b2693c3266c495781f0c2637f0090c1
Ziobeef <dimiziomikhael@gmail.com>
2026-02-11 00:26:57 +0700

    feat(debug): add like button and product search functionality with password visibility toggle

--- src/components/debug.js
+++ src/components/debug.js
@@ -2,6 +2,10 @@ import React, { useState } from "react";
 const Debug = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
+  const [likes, setLikes] = useState(1);
+  const [keyword, setKeyword] = useState("");
+  const [show, setShow] = useState(false);
+  const [pass, setPass] = useState("rahasia123");
   const handleDelete = async () => {
     const res = await fetch(`https://jsonplaceholder.typicode.com/users/1`, {
       method: "DELETE",
@@ -22,6 +26,29 @@ const Debug = () => {
     const data = await res.json();
     alert("Hasil: " + JSON.stringify(data));
   };
+  const addLike = async () => {
+    const newLikes = likes * 100;
+    await fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "PATCH",
+      body: JSON.stringify({ reactions: newLikes }),
+      headers: { "Content-type": "application/json; charset=UTF-8" },
+    });
+    setLikes(newLikes);
+  };
+
+  const search = async () => {
+    try {
+      const res = await fetch("https://dummyjson.com/products/search?q=" + keyword, {
+        method: "GET",
+
+        headers: { "Content-type": "application/json" },
+      });
+      const data = await res.json();
+      console.log(data);
+    } catch (err) {
+      alert("Error: " + err.message);
+    }
+  };
 
   return (
     <>
@@ -36,6 +63,31 @@ const Debug = () => {
         <h2>40. Registrasi </h2>
         <input placeholder="Email (eve.holt@reqres.in)" onChange={(e) => setEmail(e.target.value)} />
         <button onClick={register}>Daftar</button>
+      </div>
+
+      <div style={{ border: "2px solid black", padding: "20px", margin: "10px" }}>
+        <h2>41. Like Button</h2>
+        <p>Jumlah Likes: {likes}</p>
+        <button onClick={addLike}>👍 Tambah Like</button>
+        <p>
+          <i>Tips: Apakah jadinya 11 atau 101?</i>
+        </p>
+      </div>
+
+      <div style={{ border: "2px solid red", padding: "20px", margin: "10px" }}>
+        <h2>44. Cari Produk </h2>
+        <input onChange={(e) => setKeyword(e.target.value)} placeholder="Cari..." />
+        <button onClick={search}>Search</button>
+      </div>
+
+      <div style={{ border: "2px solid purple", padding: "20px", margin: "10px" }}>
+        <h2>51. Intip Password (Bug: Logic)</h2>
+
+        <input type={!show ? "password" : "text"} value={pass} onChange={(e) => setPass(e.target.value)} />
+
+        <button onClick={() => setShow(!show)}>{show ? "Sembunyikan" : "Tampilkan"}</button>
+
+        <p>Status: Mode {show ? "Terbuka" : "Tertutup"}</p>
       </div>
     </>
   );
