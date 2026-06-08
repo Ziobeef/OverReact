b2a32c73743c50dbd4d09299507c2de881f397c5
Ziobeef <dimiziomikhael@gmail.com>
2026-06-08 17:27:17 +0700

    feat(crud): update CRUD functionality to manage users with name, username, and email

--- src/components/crud.js
+++ src/components/crud.js
@@ -3,20 +3,42 @@ import axios from "axios";
 
 function Crud() {
   const [data, setData] = useState([]);
-  const [title, setTitle] = useState("");
-  const [body, setBody] = useState("");
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
   const handleSubmit = async () => {
-    const response = await axios.post("https://jsonplaceholder.typicode.com/posts", {
-      title: title,
-      body: body,
+    const response = await axios.post("https://jsonplaceholder.typicode.com/users", {
+      name: name,
+      username: username,
+      email: email,
     });
+
+    setData([
+      {
+        name: response.data.name,
+        username: response.data.username,
+        email: response.data.email,
+      },
+      ...data,
+    ]);
     console.log(response);
   };
   const getData = async () => {
-    const response = await axios.get("https://jsonplaceholder.typicode.com/posts");
+    const response = await axios.get("https://jsonplaceholder.typicode.com/users");
     setData(response.data);
     console.log(response.data);
   };
+  const deleteResp = async (id) => {
+    await axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`);
+
+    const index = data.findIndex((e) => e.id === id);
+
+    if (index !== -1) {
+      const dataBaru = data.toSpliced(index, 1);
+      setData(dataBaru);
+    }
+  };
+
   useEffect(() => {
     getData();
   }, []);
@@ -24,24 +46,28 @@ function Crud() {
     <div>
       <div>
         <h1>CRUD Page</h1>
-        <input type="text" value={title} onChange={(e) => setTitle (e.target.value)} />
-        <input type="text" value={body} onChange={(e) => setBody(e.target.value)} />
-        
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+        <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+
         <button onClick={() => handleSubmit()}>Submit</button>
         <tabel className="table">
           <thead>
             <tr>
-              <th>Number</th>
-              <th>Title</th>
-              <th>Body</th>
+              <th>Name</th>
+              <th>Username</th>
+              <th>Email</th>
             </tr>
           </thead>
           <tbody>
             {data.map((c, i) => (
               <tr>
-                <td>{c.id}</td>
-                <td>{c.title}</td>
-                <td>{c.body}</td>
+                <td>{c.name}</td>
+                <td>{c.username}</td>
+                <td>{c.email}</td>
+                <button type="button" onClick={() => deleteResp(c.id)}>
+                  Delete
+                </button>
               </tr>
             ))}
           </tbody>
