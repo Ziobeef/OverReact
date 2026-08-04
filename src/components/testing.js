f4aaffb433f6c97c664b817eb3f05b7741f3d748
Ziobeef <dimiziomikhael@gmail.com>
2026-08-04 19:30:19 +0700

    fix(tests): update API endpoints and adjust data handling in Testing component

--- src/components/testing.js
+++ src/components/testing.js
@@ -11,69 +11,65 @@ function Testing() {
   const [colourl, setColourl] = useState("");
   const [ide, setIde] = useState("");
   const handleSubmit = async () => {
-    const response = await axios.post("https://crudcrud.com/api/dd97adf93f3c4645a2fee7647b5976f0/kocak", {
-
+    const response = await axios.post("https://crudcrud.com/api/cf84183837f84cbba594329c35285468/kocak", {
       name: name,
       age: age,
       colour: colour,
     });
     setData([
       {
-        id: response.data._id,
+        _id: response.data._id,
         name: response.data.name,
         age: response.data.age,
         colour: response.data.colour,
       },
       ...data,
     ]);
-    console.log(response);
+    console.log("setdata=",data);
   };
   const getData = async () => {
-    const response = await axios.get("https://crudcrud.com/api/dd97adf93f3c4645a2fee7647b5976f0/kocak");
+    const response = await axios.get("https://crudcrud.com/api/cf84183837f84cbba594329c35285468/kocak");
     setData(response.data);
-    console.log(response.data);
-  }
+    console.log("getdata=",response.data);
+  };
   const deleteHasil = async (id) => {
-    const response = await axios.delete(`https://crudcrud.com/api/dd97adf93f3c4645a2fee7647b5976f0/kocak/${id}`);
-    const index = data.findIndex((l) => l.id === id);
-     if (index !== -1) {
+    const response = await axios.delete(`https://crudcrud.com/api/cf84183837f84cbba594329c35285468/kocak/${id}`);
+    const index = data.findIndex((l) => l._id === id);
+    if (index !== -1) {
       const dataBaru = data.toSpliced(index, 1);
-      setData(dataBaru);}
+      setData(dataBaru);
+    } 
   };
   const updateHasil = async (id) => {
-    try{
-
-    const response = await axios.put(`https://crudcrud.com/api/dd97adf93f3c4645a2fee7647b5976f0/kocak/${id}`, {
-      name: namel,
-      age: agel, 
-      colour: colourl,
-      
-    });
-    console.log("response:",response);
-    if(response.status === 200){
-    getData();
-    alert("berhasil update");
-    }else{
+    try {
+      const response = await axios.put(`https://crudcrud.com/api/cf84183837f84cbba594329c35285468/kocak/${id}`, {
+        name: namel,
+        age: agel,
+        colour: colourl,
+      });
+      console.log("response:", response);
+      if (response.status === 200) {
+        getData();
+        alert("berhasil update");
+      } else {
+        alert("gagal update");
+      }
+    } catch (error) {
+      console.error("Error updating data:", error);
       alert("gagal update");
     }
-  }
-  catch(error){
-    console.error("Error updating data:", error);
-    alert("gagal update");
-  }
-}
+  };
   const updateData = async (l) => {
     setNamel(l.name);
     setAgel(l.age);
     setColourl(l.colour);
     setIde(l._id);
     console.log(l);
-  }
+  };
   useEffect(() => {
     getData();
   }, []);
 
-    
   return (
     <div>
       <div>
@@ -85,7 +81,6 @@ function Testing() {
         <input type="text" value={agel} onChange={(e) => setAgel(e.target.value)} />
         <input type="text" value={colourl} onChange={(e) => setColourl(e.target.value)} />
         <button onClick={() => updateHasil(ide)}>Done</button>
-
       </div>
       <div>
         <thead>
@@ -97,12 +92,18 @@ function Testing() {
         <tbody>
           {data.map((l, i) => (
             <tr>
-              <td>{l.id}</td>
+              <td>{l._id}</td>
               <td>{l.name}</td>
               <td>{l.age}</td>
               <td>{l.colour}</td>
-              <button type="button" onClick={() => deleteHasil(l.id)}>hapus</button>
-             <button type="button" onClick={() => updateData(l)}>update</button>
+              <td>
+                <button type="button" onClick={() => deleteHasil(l._id)}>
+                  hapus
+                </button>
+                <button type="button" onClick={() => updateData(l)}>
+                  update
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
